983876816eea3628452396a3545bc1577b7d3a5b
Baptiste Dellerue <baptiste.dellerue@orange.fr>
2026-02-09 17:38:44 +0100

    Petite correction sur les sorts

--- classes/sorts.py
+++ classes/sorts.py
@@ -6,18 +6,28 @@ class Sort_zone_carré:
         self.longueur = longueur
         self.largueur = largeur
         self.position = position
-
-    def draw(self):
-        pyglet.shapes.Rectangle(
+        self.forme = pyglet.shapes.Rectangle(
             self.position[0],
             self.position[1],
             self.longueur,
             self.largueur,
             color=(255, 153, 51, 100),
         )
+        self.forme.anchor_position = (self.longueur/2, self.largueur/2)
+
+    def draw(self):
+        self.forme.draw()
 
     def move(self, new_position):
         self.position = new_position
+        self.forme = pyglet.shapes.Rectangle(
+            self.position[0],
+            self.position[1],
+            self.longueur,
+            self.largueur,
+            color=(255, 153, 51, 100),
+        )
+        self.forme.anchor_position = (self.longueur/2, self.largueur/2)
 
 
 class Sort_zone_cercle:
